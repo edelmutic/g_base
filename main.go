8995ab09f6bd9e58a8ab19061812b021d68692b7
edelmutic <edelmutic@gmail.com>
2021-05-12 21:25:15 +0300

    add fib func

--- main.go
+++ main.go
@@ -20,5 +20,17 @@ func handleFib(w http.ResponseWriter, r *http.Request)  {
 http.Error(w, err.Error(), http.StatusBadRequest)
 return
 	 }
-	io.WriteString(w, strconv.Itoa()) 
+	io.WriteString(w, strconv.Itoa(fib(n))) 
+}
+
+func fib(n int) int  {
+	if n <= 1 {
+		return n
+	}
+	if r, ok := cache[n]; ok  {
+		return r
+	}
+	sum:= fib(n-1) + fib(n-2)
+	cache:= sum
+	return sum
 }
