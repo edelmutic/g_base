20fbe0ec40cf1e137b6675c2bc9fae6f4b1eedac
edelmutic <edelmutic@gmail.com>
2021-05-14 22:48:44 +0300

    add test file

--- main_test.go
+++ main_test.go
@@ -1,26 +1,33 @@
-package main
+package myserver
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
+func TestMyHandler(t *testing.T) {
+	handler := &MyHandler{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
 
-func TestHandleFib(t *testing.T) {
-testCase:= []struct {
-	name string
-	num int
-	want []byte
-} {
-	{
-	name: "zero",
-	num: 0,
-	want: []byte("0"),
-}, {
-	name: "one",
-	num: 1,
-	want: []byte("1"),
-},
-}
-handler := http.HandlerFunc(handleFib)
+	for _, i := range []int{1, 2} {
+		resp, err := http.Get(server.URL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.StatusCode != 200 {
+			t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
+		}
+		expected := fmt.Sprintf("Visitor count: %d.", i)
+		actual, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if expected != string(actual) {
+			t.Errorf("Expected the message '%s'\n", expected)
+		}
+	}
 }
